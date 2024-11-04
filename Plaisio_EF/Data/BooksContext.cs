908860136e221a8843fad62d831855b1400e0c8a
Stan K <sk2114@outlook.com>
2024-11-04 12:31:38 +0200

    Add project files.

---
+++ Plaisio_EF/Data/BooksContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Plaisio_EF.Domain;
+
+namespace Plaisio_EF.Data
+{
+    public class BooksContext : DbContext
+    {
+        DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+                ("Data Source = 192.168.1.89; Initial Catalog = Plaisio_EF; User ID='sa'; Password = '!Abc123456'; Encrypt = false;"); //Integrated Security = true;
+        }
+    }
+}
---
+++ Plaisio_EF/Domain/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plaisio_EF.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; } //or BookId
+        public string? Title { get; set; }
+        public DateTime? PubDate { get; set; }
+        public int? Pages { get; set; }
+    }
+}
